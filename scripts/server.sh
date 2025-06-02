621bb14757484fbdccd199bfb9eb39bc0bbb4218
renegadespork <jessekilburn@gmail.com>
2025-06-01 20:31:56 -0700

    Removed code. See README

--- scripts/server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /server/factorio
-# Run server
-./bin/x64/factorio --start-server /saves/$MAP_NAME.zip --config /config/config.ini --server-settings /config/server-settings.json
--- scripts/update.sh
+++
@@ -1,8 +0,0 @@
-# Download and install server
-echo "Downloading latest server binary..."
-curl -L --max-redirs 1 "https://factorio.com/get-download/stable/headless/linux64" -o /server/factorio-dedicated-server.tar.xz && \
-tar -xf /server/factorio-dedicated-server.tar.xz -C /server && \
-rm /server/factorio-dedicated-server.tar.xz
-# Organize config files
-sed -i 's/__PATH__executable__\/..\/..\/config/\/config/g' /server/factorio/config-path.cfg && \
-echo "Done."
